9653c5f46c88081503f4fe416c1cdd8fb8f9b210
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2025-02-15 13:36:57 -0300

    CSS without responsive

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 
+
 const webpackConfig = require("./webpack.config.js");
 
 const paths = {
@@ -16,13 +17,16 @@ const paths = {
     watch: "src/ts/**/*.ts",
   },
   styles: {
-    src: "src/scss/main.scss",
+    src: "src/scss/**/*.scss",
   },
   img: {
     src: "src/img/**/*",
   },
   html: {
     src: "src/index.html",
+  },
+  fonts:{
+    src: "src/fonts/**/*"
   },
   dest: "dist",
   temp: ".tmp",
@@ -86,7 +90,12 @@ function img() {
   return src(paths.img.src).pipe(dest(paths.dest + "/img"));
 }
 
-const build = series(clean, parallel(styles, scripts, html, img));
+function fonts() {
+  return src(paths.fonts.src)
+      .pipe(dest('dist/fonts'));
+}
+
+const build = series(clean, parallel(styles, scripts, html, img, fonts));
 const dev = () => {
   watch(paths.scripts.watch, { ignoreInitial: false }, scripts).on(
     "change",
@@ -94,6 +103,7 @@ const dev = () => {
   );
   watch(paths.styles.src, { ignoreInitial: false }, styles);
   watch(paths.img.src, { ignoreInitial: false }, img);
+  watch(paths.fonts.src, { ignoreInitial: false }, fonts);
   watch(paths.html.src, { ignoreInitial: false }, html).on(
     "change",
     browserSync.reload
